d04ba08ecdb418df30071e19ed494e0aa2cd3aff
Jordy <jordanwright84@gmail.com>
2021-10-31 17:25:43 -0500

    flask stuff

---
+++ be/app.py
@@ -0,0 +1,15 @@
+#### Some sample code to get us started
+#### Use "pip install flask" to get it on your computer
+#### Useful documentation: https://flask.palletsprojects.com/en/2.0.x/quickstart/#a-minimal-application
+
+
+# from flask import Flask
+
+# app = Flask(__name__)
+
+# @app.route('/')
+# def index():
+#     return 'Hello world'
+
+# if __name__ == '__main__':
+#     app.run(debug=True, host='0.0.0.0')
