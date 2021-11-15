3095cbcb68ca088d41b9bcd8ea66ec4c5fdf7b44
hessej25 <hessej25@gmail.com>
2021-11-15 17:50:48 -0600

    Created zoom in and out function
    Function is located in index.js file. Using "react-zoom-pan-pinch" Image is currently not appearing on site when ran.

--- fe/src/index.js
+++ fe/src/index.js
@@ -5,6 +5,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { FontAwesomeIcon } from "react-fontawesome";
+import { TransformWrapper, TransformComponent } from "react-zoom-pan-pinch";
+import maplayout from './layout.png';
 
 ReactDOM.render(
   <React.StrictMode>
@@ -12,6 +15,53 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
+
+const ImageZoom = () => {
+  return (
+    <>
+      <div className="row">
+        <div className="col text-center">
+          <h2>Zoomable Image</h2>
+          <p className="mt-3">
+            <div className="row mt-3 d-flex justify-content-center">
+              <div className="col-lg-8 text-center text-dark">
+                <TransformWrapper
+                  defaultScale={1}
+                  defaultPositionX={1}
+                  defaultPositionY={1}
+                >
+                  {({ zoomIn, zoomOut, ...rest }) => (
+                    <>
+                      <div className="col-lg-12 text-center mb-3">
+                        <button
+                          className="btn btn-outline-primary mr-2"
+                          onClick={zoomIn}
+                        >
+                          <FontAwesomeIcon icon="plus" /> Zoom In
+                        </button>
+                        <button
+                          className="btn btn-outline-secondary"
+                          onClick={zoomOut}
+                        >
+                          <FontAwesomeIcon icon="minus" /> Zoom Out
+                        </button>
+                      </div>
+                      <TransformComponent>
+                        <img src={maplayout} alt = "testmap" style={{ width: "100%" }} />
+                      </TransformComponent>
+                    </>
+                  )}
+                </TransformWrapper>
+              </div>
+            </div>
+          </p>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default ImageZoom;
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
