f87bb904f528eabcf8aeb08e2ad2df90ff20da4b
Kevin Crabbe <kevincrabbe99@gmail.com>
2021-11-18 14:25:48 -0600

    add dotenv package

--- fe/src/util/Endpoints.js
+++ fe/src/util/Endpoints.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 
 export async function test() {
-    const response = await axios.get(`${process.env.REACT_APP_ITIFN_API}/test`);
+    const response = await axios.get(`${process.env.REACT_APP_ITIFN_API}test`);
     console.log(response.data);
     return response.data;
 }
