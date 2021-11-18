38a950053ded9ac868e4d829039baa105dd354d3
Kevin Crabbe <kevincrabbe99@gmail.com>
2021-11-17 20:17:25 -0600

    Working connection on localhost env

--- fe/src/pages/map/Map.js
+++ fe/src/pages/map/Map.js
@@ -1,7 +1,12 @@
 import React from 'react'
 import RoutesView from '../../components/Routes/RoutesView'
 
+import { test } from '../../util/Endpoints'
+
 export default function Map() {
+
+    test();
+
     return (
         <div>
             <RoutesView />
