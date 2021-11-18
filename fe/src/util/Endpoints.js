5f4e33841f6a2f5041dc93b81021854d60f369e2
Kevin Crabbe <kevincrabbe99@gmail.com>
2021-11-18 13:10:17 -0600

    Added localhost var to fe

--- fe/src/util/Endpoints.js
+++ fe/src/util/Endpoints.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 
 export async function test() {
-    const response = await axios.get('http://localhost:8080/test');
+    const response = await axios.get(`${process.env.REACT_APP_ITIFN_API}/test`);
     console.log(response.data);
     return response.data;
 }
