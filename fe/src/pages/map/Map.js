9751cc0eef4b054d16bd3f5f32a3c43b95a62814
hessej25 <hessej25@gmail.com>
2021-11-30 11:48:46 -0600

    ImageZoom.js made instead of in index.js

--- fe/src/pages/map/Map.js
+++ fe/src/pages/map/Map.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import RoutesView from '../../components/Routes/RoutesView'
+import ImageZoom from '../../components/panZoom/ImageZoom'
 
 export default function Map() {
     return (
         <div>
             <RoutesView />
+            <ImageZoom/>
         </div>
     )
 }
