a4d7e72191049bc5decbb5a65be9bc9e88e0f363
jordytheboy <70452593+jordytheboy@users.noreply.github.com>
2021-12-01 18:40:38 -0600

    added CORS

--- be/app.py
+++ be/app.py
@@ -1,13 +1,14 @@
 from flask import Flask, Response, json
-from flask_cors import CORS
+from flask_cors import CORS, cross_origin
 from api import navigator_api
-from app.mongo_connection import MongoConnection
 
 app = Flask(__name__)
-CORS(app)
 app.register_blueprint(navigator_api)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
 
 @app.route('/test')
+@cross_origin()
 def index():
     return Response(response=json.dumps({"Status": "UP"}),
                     status=200,
