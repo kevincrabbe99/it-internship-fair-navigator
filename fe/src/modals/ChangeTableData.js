e84960acd9ddf184ceda117b133674389736befb
knstrom <knstrom@ilstu.edu>
2021-12-01 18:42:05 -0600

    updated input form

--- fe/src/modals/ChangeTableData.js
+++ fe/src/modals/ChangeTableData.js
@@ -8,11 +8,12 @@ export class DataForm extends React.Component{
         this.tableId = tableId;
 
         this.getTableInfo = this.getTableInfo.bind(this);
-        this.changeCompany = this.changeCompany.bind(this);
+        this.change = this.change.bind(this);
         this.previewChanges = this.previewChanges.bind(this);
         this.submit = this.submit.bind(this);
 
         this.table = JSON.parse(this.getTableInfo(tableId));
+        this.company = JSON.parse(this.table.company)
     }
 
     getTableInfo(tableId){
@@ -21,7 +22,7 @@ export class DataForm extends React.Component{
         return response;
     }
 
-    changeCompany(event){
+    change(event){
         this.setState({value: event.target.value});
     }
 
@@ -31,8 +32,24 @@ export class DataForm extends React.Component{
                 <form onSubmit = {this.submit}>
                     <label>
                         Company Name:
-                        <textarea id = "companyName" name = {this.table.company} rows = {1} cols = {50} />
+                        <textarea id = "companyName" name = {this.company._name} rows = {1} cols = {50} />
                     </label>
+                    <br />
+                    <label>
+                        Number of Representatives:
+                        <textarea id = "numberOfReps" name = {this.company._number_of_reps} rows = {1} cols = {50} />
+                    </label>
+                    <br />
+                    <label>
+                        Company Website:
+                        <textarea id = "companyWebsite" name = {this.company._website} rows = {1} cols = {50} />
+                    </label>
+                    <br />
+                    <label>
+                        Notes:
+                        <textarea id = "notes" name = {this.company._other_info} rows = {1} cols = {50} />
+                    </label>
+                    <br />
                     <input type = "button" value = {"Edit"} onClick = {this.render} />
                     <input type = "submit" value = "Submit" />
                 </form>
@@ -53,7 +70,22 @@ export class DataForm extends React.Component{
                 <form onSubmit = {this.submit}>
                     <label>
                         Company Name:
-                        <input type = "text" value = {this.table.company} onChange = {this.changeCompany} />
+                        <input type = "text" value = {this.company._name} onChange = {this.change} />
+                    </label>
+                    <br />
+                    <label>
+                        Number of Representatives:
+                        <input type = "text" value = {this.company._number_of_reps} onChange = {this.change} />
+                    </label>
+                    <br />
+                    <label>
+                        Company Website:
+                        <input type = "text" value = {this.company._website} onChange = {this.change} />
+                    </label>
+                    <br />
+                    <label>
+                        Notes:
+                        <input type = "text" value = {this.company._other_info} onChange = {this.change} />
                     </label>
                     <br />
                     <label>
