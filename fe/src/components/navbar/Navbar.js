68e794e6291cd9f590fc1399b12ebf25c1257d36
knstrom <knstrom@ilstu.edu>
2021-12-03 18:16:58 -0600

    Preview Changes

--- fe/src/components/navbar/Navbar.js
+++ fe/src/components/navbar/Navbar.js
@@ -3,7 +3,7 @@ import * as FaIcons from 'react-icons/fa';
 import { Link } from 'react-router-dom';
 import './Navbar.css';
 import { isAdmin, UserContext } from '../../contexts/userContext.js';
-import { addNewYear } from '../../util/Endpoints.js';
+import { addNewYear, getTablesEndpoint, updateTableEndpoint } from '../../util/Endpoints.js';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Modal } from 'react-bootstrap';
 import { Button } from 'react-bootstrap';
@@ -11,6 +11,9 @@ import { YearContext } from '../../contexts/yearContext.js';
 import { structureYearState } from '../../contexts/yearContext.js';
 import { getAvailableYears } from '../../util/Endpoints.js';
 import { SidebarContext } from '../../contexts/sidebarContext.js';
+import { createTableEndpoint } from '../../util/Endpoints.js';
+import { MdAirlineSeatLegroomExtra } from 'react-icons/md';
+import { arrow } from '@popperjs/core';
 import { RoutesContext } from '../../contexts/routesContext';
 
 function Navbar() {
@@ -26,6 +29,7 @@ function Navbar() {
   const [modalShowing, setModalShowing] = useState(false);
   const [showAddYearModal, setShowAddYearModal] = useState(false);
   const [showCreateTableModal, setShowCreateTableModal] = useState(false);
+  const [showPreview, setShowPreview] = useState(false);
   
   const [availableYears, setAvailableYears] = useState([]);
   const [newYearValue, setNewYearValue] = useState(null);
@@ -110,7 +114,7 @@ function Navbar() {
     <>
       <li>
         <div className='button' onClick={() => setShowCreateTableModal(true)}> 
-          CREATE TABLE
+          MAKE TABLE
         </div>
       </li>
       <li>
@@ -141,25 +145,41 @@ function Navbar() {
     setRoutesContext({...routesContext, showing: !routesContext.showing}) 
   }
 
+  const [submitCreateMap, setSubmitCreateMap] = useState(false);
+  const [id, setId] = useState(null);
   const setYearTo = (val) => {
     setYearData(structureYearState(val, yearData.available))
   }
 
-  const [tableId, setTableId] = useState(null);
   const [x, setX] = useState(null);
   const [y, setY] = useState(null);
-  const [name, setName] = useState(null);
+  const [cName, setName] = useState(null);
   const [numReps, setNumReps] = useState(null);
   const [website, setWebsite] = useState(null);
   const [notes, setNotes] = useState(null);
-  const [year, setYear] = useState(null);
+  const [logoFile, setLogo] = useState(null);
 
-  function submit(){
-    const url = "api.itfnavigator.com/api/navigator/" + 
-        "{_id: " + tableId + ", _x_coord: " + x + ", _y_coord: " + y + ", _company: {_name: " + name + ", _number_of_reps: " + numReps + ",_website: " + website + ", _other_info: " + notes + "}, _year: " + year + "}";
-    const response = fetch(url);
-    return response;
-  }
+  useEffect(() => {
+    async function makeTableAsyncWrapper(){
+      console.log("async admin working");
+      const getTables = await getTablesEndpoint(user.uuid);
+      var response;
+      if(getTables.some(item => item.x_coord === x && item.y_coord === y)){
+        var id = getTables.results.find(item => item.x_coord === x && item.y_coord === y);
+        response = await updateTableEndpoint(user.uuid, id, x, y, cName, numReps, website, notes, '2021');
+      }
+      else{
+        response = await createTableEndpoint(user.uuid, x, y, cName, numReps, website, notes, '2021');
+      }
+      
+      return response;
+    }
+    console.log("async working", user);
+    if(user && user.uuid){
+      makeTableAsyncWrapper();
+    }
+  }, [submitCreateMap])
+  
 
   return (
     <>
@@ -236,6 +256,7 @@ function Navbar() {
             {
                 isAdmin() &&
                 showCreateTableModal &&
+                !showPreview &&
                 <Modal.Dialog>
                   <Modal.Header>
                     <Modal.Title>Create a Table</Modal.Title>
@@ -243,49 +264,133 @@ function Navbar() {
 
                   <Modal.Body>
                     <div>
-                      <form onSubmit = {''}>
-                          <div id = "inputLabel">
+                          <div>
                               <label>
                                   Company Name:
-                                  <input type = "text" value = {name} onChange = {e => setName(e.target.value)} />
+                                  <input type = "text" value = {cName} onChange = {e => setName(e.target.value)} />
                               </label>
                           </div>
                           <br />
-                          <div id = "inputLabel">
+                          <div>
                               <label>
                                   Number of Representatives:
                                   <input type = "text" value = {numReps} onChange = {e => setNumReps(e.target.value)} />
                               </label>
                           </div>
                           <br />
-                          <div id = "inputLabel">
+                          <div>
                               <label>
                                   Company Website:
                                   <input type = "text" value = {website} onChange = {e => setWebsite(e.target.value)} />
                               </label>
                           </div>
                           <br />
-                          <div id = "inputLabel">
+                          <div>
                               <label>
                                   Notes:
                                   <input type = "text" value = {notes} onChange = {e => setNotes(e.target.value)} />
                               </label>
                           </div>
                           <br />
-                          <div id = "inputLabel">
+                          <div>
                               <label>
-                                  Company Logo:
-                                  <input type = "file" />
+                                  x-Coordinate:
+                                  <input type = "text" value = {x} onChange = {e => setX(e.target.value)} />
                               </label>
                           </div>
                           <br />
-                      </form>
+                          <div>
+                              <label>
+                                  y-Coordinate:
+                                  <input type = "text" value = {y} onChange = {e => setY(e.target.value)} />
+                              </label>
+                          </div>
+                          <br />
+                          <div>
+                              <label>
+                                  Company Logo:
+                                  <input type = "text" value = {logoFile} onChange = {e => setLogo(e.target.value)}/>
+                              </label>
+                          </div>
+                          <br />
                     </div>
                   </Modal.Body>
 
                   <Modal.Footer>
                     <Button variant="secondary" onClick={() => setShowCreateTableModal(false)}>CLOSE</Button>
-                    <Button variant="primary" onClick = {submit()}>CREATE</Button>
+                    <Button variant="tertiary" onClick={() => setShowPreview(!showPreview)}>PREVIEW</Button>
+                    <Button variant="primary" onClick = {() => setSubmitCreateMap(!submitCreateMap)}>SUBMIT</Button>
+                  </Modal.Footer>
+                </Modal.Dialog>
+            }
+
+            {
+                isAdmin() &&
+                showCreateTableModal &&
+                showPreview &&
+                <Modal.Dialog>
+                  <Modal.Header>
+                    <Modal.Title>View Table</Modal.Title>
+                  </Modal.Header>
+
+                  <Modal.Body>
+                    <div>
+                          <div>
+                              <label>
+                                  Company Name:
+                                  <p>{cName}</p>
+                              </label>
+                          </div>
+                          <br />
+                          <div>
+                              <label>
+                                  Number of Representatives:
+                                  <p>{numReps}</p>
+                              </label>
+                          </div>
+                          <br />
+                          <div>
+                              <label>
+                                  Company Website:
+                                  <p>{website}</p>
+                              </label>
+                          </div>
+                          <br />
+                          <div>
+                              <label>
+                                  Notes:
+                                  <p>{notes}</p>
+                              </label>
+                          </div>
+                          <br />
+                          <div>
+                              <label>
+                                  x-Coordinate:
+                                  <p>{x}</p>
+                              </label>
+                          </div>
+                          <br />
+                          <div>
+                              <label>
+                                  y-Coordinate:
+                                  <p>{y}</p>
+                              </label>
+                          </div>
+                          <br />
+                          <div>
+                              <label>
+                                  Company Logo:
+                                  <img src={logoFile} alt = 'Company Logo' width = '70' height = '70' />
+                              </label>
+                          </div>
+                          <br />
+                    </div>
+                  </Modal.Body>
+
+                  <Modal.Footer>
+                    <Button variant="secondary" onClick={() => setShowCreateTableModal(false)}>CLOSE</Button>
+                    <Button variant="tertiary" onClick={() => setShowPreview(!showPreview)}>EDIT</Button>
+                    <Button variant="primary" onClick = {() => setSubmitCreateMap(!submitCreateMap)}>SUBMIT</Button>
                   </Modal.Footer>
                 </Modal.Dialog>
             }
