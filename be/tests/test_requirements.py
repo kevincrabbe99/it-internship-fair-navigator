8ccee61b16da1684c741d385d2f30534dfc40b64
jordytheboy <70452593+jordytheboy@users.noreply.github.com>
2021-12-03 23:05:43 -0600

    Fixed url

--- be/tests/test_requirements.py
+++ be/tests/test_requirements.py
@@ -10,13 +10,13 @@ import pytest
 # Signup for Email List
 def test_signup_valid_email():
     email_json = {'email': 'test_email@gmail.com'}
-    res = requests.put('http://localhost:8080/api/navigator/subscribe', json=email_json)
+    res = requests.put('https://api.itfnavigator.com/api/navigator/subscribe', json=email_json)
 
     assert res.text == "Thanks for signing up!", "Test Failed"
 
 def test_signup_invalid_email():
     email_json = {'email': 'asdf'}
-    res = requests.put('http://localhost:8080/api/navigator/subscribe', json=email_json)
+    res = requests.put('https://api.itfnavigator.com/api/navigator/subscribe', json=email_json)
 
     assert res.text == "Invalid email", "Test Failed"
 
@@ -24,19 +24,19 @@ def test_signup_invalid_email():
 # Unsubscribe from Email List
 def test_unsubscribe_valid_email():
     email_json = {'email': 'test_email@gmail.com'}
-    res = requests.delete('http://localhost:8080/api/navigator/unsubscribe', json=email_json)
+    res = requests.delete('https://api.itfnavigator.com/api/navigator/unsubscribe', json=email_json)
 
     assert res.text == "Unsubscribed!", "Test Failed"
 
 def test_unsubscribe_valid_email_not_found():
     email_json = {'email': 'not_in_the_database@gmail.com'}
-    res = requests.delete('http://localhost:8080/api/navigator/unsubscribe', json=email_json)
+    res = requests.delete('https://api.itfnavigator.com/api/navigator/unsubscribe', json=email_json)
 
     assert res.text == "Email not found", "Test Failed"
 
 def test_unsubscribe_invalid_email():
     email_json = {'email': 'thisisnotarealemailaddress'}
-    res = requests.delete('http://localhost:8080/api/navigator/unsubscribe', json=email_json)
+    res = requests.delete('https://api.itfnavigator.com/api/navigator/unsubscribe', json=email_json)
 
     assert res.text == "Invalid email", "Test Failed"
 
@@ -48,15 +48,15 @@ def test_login_good_creds():
     navpwd = os.getenv('NAVPWD')
     data = {'username': navuser, 'password': navpwd}
 
-    res = requests.post('http://localhost:8080/api/navigator/login', json=data)
+    res = requests.post('https://api.itfnavigator.com/api/navigator/login', json=data)
     res_json = res.json()
     assert '_id' in res_json, "Test Failed"
-    res = requests.delete('http://localhost:8080/api/navigator/logout', json={'sessionUUID': res_json['uuid']})
+    res = requests.delete('https://api.itfnavigator.com/api/navigator/logout', json={'sessionUUID': res_json['uuid']})
 
 def test_login_bad_creds():
     data = {'username': 'wrong_user', 'password': 'wrong_pass'}
 
-    res = requests.post('http://localhost:8080/api/navigator/login', json=data)
+    res = requests.post('https://api.itfnavigator.com/api/navigator/login', json=data)
     res.status_code
     assert  res.status_code == 401, "Test Failed"
 
@@ -68,9 +68,9 @@ def test_logout():
     navpwd = os.getenv('NAVPWD')
     data = {'username': navuser, 'password': navpwd}
 
-    res = requests.post('http://localhost:8080/api/navigator/login', json=data)
+    res = requests.post('https://api.itfnavigator.com/api/navigator/login', json=data)
     res_json = res.json()
-    res = requests.delete('http://localhost:8080/api/navigator/logout', json={'sessionUUID': res_json['uuid']})
+    res = requests.delete('https://api.itfnavigator.com/api/navigator/logout', json={'sessionUUID': res_json['uuid']})
     assert  res.status_code == 200, "Test Failed"
 
 # Requirement 18
@@ -81,7 +81,7 @@ def test_remove_table_correct_credentials_existing_table():
     navpwd = os.getenv('NAVPWD')
     data = {'username': navuser, 'password': navpwd}
 
-    res = requests.post('http://localhost:8080/api/navigator/login', json=data)
+    res = requests.post('https://api.itfnavigator.com/api/navigator/login', json=data)
     res_json = res.json()
     uuid = res_json['uuid']
 
@@ -92,13 +92,13 @@ def test_remove_table_correct_credentials_existing_table():
                                                             'website': 'test.org', 
                                                             'other_info': 'none'}
                                                             ,'year': '2021'}
-    res = requests.put('http://localhost:8080/api/navigator/table', json=table_to_add, headers=header)
+    res = requests.put('https://api.itfnavigator.com/api/navigator/table', json=table_to_add, headers=header)
 
     table_to_delete = res.json()['tables'][-1]['_id']
     print(table_to_delete)
     res = requests.delete('http://localhost:8080/api/navigator/table', json={'_id': table_to_delete, 'year': '2021'}, headers=header)
 
-    requests.delete('http://localhost:8080/api/navigator/logout', json={'sessionUUID': uuid})
+    requests.delete('https://api.itfnavigator.com/api/navigator/logout', json={'sessionUUID': uuid})
 
     assert res.status_code == 200, "Test Failed"
 
@@ -108,16 +108,16 @@ def test_remove_table_correct_credentials_nonexisting_table():
     navpwd = os.getenv('NAVPWD')
     data = {'username': navuser, 'password': navpwd}
 
-    res = requests.post('http://localhost:8080/api/navigator/login', json=data)
+    res = requests.post('https://api.itfnavigator.com/api/navigator/login', json=data)
     res_json = res.json()
     uuid = res_json['uuid']
 
     header = {'Authorization': uuid}
 
     table_to_delete = "nonexisting_table_id"
-    res = requests.delete('http://localhost:8080/api/navigator/table', json={'_id': table_to_delete, 'year': '2021'}, headers=header)
+    res = requests.delete('https://api.itfnavigator.com/api/navigator/table', json={'_id': table_to_delete, 'year': '2021'}, headers=header)
 
-    requests.delete('http://localhost:8080/api/navigator/logout', json={'sessionUUID': uuid})
+    requests.delete('https://api.itfnavigator.com/api/navigator/logout', json={'sessionUUID': uuid})
 
     assert res.status_code == 400, "Test Failed"
 
@@ -128,6 +128,6 @@ def test_remove_table_no_credentials():
     data = {'username': navuser, 'password': navpwd}
 
     table_to_delete = "nonexisting_table_id"
-    res = requests.delete('http://localhost:8080/api/navigator/table', json={'_id': table_to_delete, 'year': '2021'})
+    res = requests.delete('https://api.itfnavigator.com/api/navigator/table', json={'_id': table_to_delete, 'year': '2021'})
     
     assert res.status_code == 401, "Test Failed"
